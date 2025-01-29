6b31561ac51809dd016530de14c4fe36968d93f5
KavinSandking <150557351+KavinSandking@users.noreply.github.com>
2025-01-28 22:25:31 -0600

    1. Auton - updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
@@ -175,7 +176,7 @@ public class Auton extends LinearOpMode {
 
                 long elapsedTime = System.currentTimeMillis() - startTime;
 
-                if (elapsedTime >= 0) {
+                if (elapsedTime >= 150){
                     return false;
                 }
                 return true;
@@ -200,7 +201,7 @@ public class Auton extends LinearOpMode {
 
                 long elapsedTime = System.currentTimeMillis() - startTime;
 
-                if (elapsedTime >= 350) {
+                if (elapsedTime >= 450) {
                     return false;
                 }
                 return true;
@@ -226,7 +227,7 @@ public class Auton extends LinearOpMode {
 
                 long elapsedTime = System.currentTimeMillis() - startTime;
 
-                if (elapsedTime >= 250) {
+                if (elapsedTime >= 350) {
                     return false;
                 }
                 return true;
@@ -271,15 +272,22 @@ public class Auton extends LinearOpMode {
         }
 
         public class OpenClaw implements Action {
+            private long startTime;
             private boolean initialized = false;
 
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
                     claw.setPosition(0.0);
+                    startTime = System.currentTimeMillis();
                     initialized = true;
                 }
-                return false;
+                long elapsedTime = System.currentTimeMillis() - startTime;
+
+                if (elapsedTime >= 350) {
+                    return false;
+                }
+                return true;
             }
         }
 
@@ -297,35 +305,22 @@ public class Auton extends LinearOpMode {
         elbow elbow = new elbow(hardwareMap);
 
         TrajectoryActionBuilder Sample1 = drive.actionBuilder(initialPose)
-                .splineTo(new Vector2d(6.42, 23.72), Math.toRadians(134.13),new TranslationalVelConstraint(85));
-        TrajectoryActionBuilder GOBACK = Sample1.endTrajectory().fresh()
-                .lineToX(11.541);
-        TrajectoryActionBuilder Sample2 = GOBACK.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(24.34, 14.13), Math.toRadians(-4.19));
-        TrajectoryActionBuilder Wait = GOBACK.endTrajectory().fresh()
-                .waitSeconds(2.0);
+                .splineTo(new Vector2d(5.33, 23.78), Math.toRadians(133.98),new TranslationalVelConstraint(85));
+        TrajectoryActionBuilder Sample2 = Sample1.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(22.98, 16.02), Math.toRadians(-0.57));
         TrajectoryActionBuilder DROP2 = Sample2.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(10.01, 18.55), Math.toRadians(134.13),new TranslationalVelConstraint(85));
-        TrajectoryActionBuilder MOVEFORWARD = DROP2.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(8.17, 24.46), Math.toRadians(130.17),new TranslationalVelConstraint(85));
-        TrajectoryActionBuilder GOBACK2 = MOVEFORWARD.endTrajectory().fresh()
-                .lineToX(11.541);
-        TrajectoryActionBuilder Sample3 = GOBACK2.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(24.49, 28.81), Math.toRadians(-7.09));
+                .strafeToLinearHeading(new Vector2d(5.33, 23.78), Math.toRadians(133.98),new TranslationalVelConstraint(85));
+        TrajectoryActionBuilder Sample3 = DROP2.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(22.81, 26.81), Math.toRadians(-1.29));
         TrajectoryActionBuilder DROP3 = Sample3.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(10.01, 18.55), Math.toRadians(134.13),new TranslationalVelConstraint(85));
-        TrajectoryActionBuilder MOVEFORWARD3 = DROP3.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(8.17, 24.46), Math.toRadians(130.17),new TranslationalVelConstraint(85));
-        TrajectoryActionBuilder GOBACK3 = MOVEFORWARD3.endTrajectory().fresh()
-                .lineToX(11.541);
-        TrajectoryActionBuilder Sample4 = GOBACK3.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(26.46, 27.38), Math.toRadians(34.31));
+                .strafeToLinearHeading(new Vector2d(5.33, 23.78), Math.toRadians(133.98),new TranslationalVelConstraint(85));
+        TrajectoryActionBuilder Sample4 = DROP3.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(24.81, 28.12), Math.toRadians(29.84));
         TrajectoryActionBuilder DROP4 = Sample4.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(10.01, 18.55), Math.toRadians(134.13),new TranslationalVelConstraint(85));
-        TrajectoryActionBuilder MOVEFORWARD4 = DROP4.endTrajectory().fresh()
-                .strafeToLinearHeading(new Vector2d(8.17, 24.46), Math.toRadians(130.17),new TranslationalVelConstraint(85));
-        TrajectoryActionBuilder GOBACK4 = MOVEFORWARD4.endTrajectory().fresh()
-                .lineToX(11.541);
+                .strafeToLinearHeading(new Vector2d(5.33, 23.78), Math.toRadians(133.98),new TranslationalVelConstraint(85));
+        TrajectoryActionBuilder goBack = DROP4.endTrajectory().fresh()
+                .strafeToLinearHeading(new Vector2d(17.69, 17.68), Math.toRadians(143.1));
+
 
 
 
@@ -336,20 +331,13 @@ public class Auton extends LinearOpMode {
 
 
         Action Sample_1 = Sample1.build();
-        Action goBack = GOBACK.build();
         Action Sample_2 = Sample2.build();
-        Action moveForward = MOVEFORWARD.build();
-        Action moveForward3 = MOVEFORWARD3.build();
-        Action moveForward4 = MOVEFORWARD4.build();
-        Action WAIT = Wait.build();
         Action Drop2 = DROP2.build();
-        Action goBack2 = GOBACK2.build();
         Action Sample_3 = Sample3.build();
         Action Drop3 = DROP3.build();
-        Action goBack3 = GOBACK3.build();
         Action Sample_4  = Sample4.build();
         Action Drop4 = DROP4.build();
-        Action goBack4 = GOBACK4.build();
+        Action goBack4 = goBack.build();
 
 
         waitForStart();
@@ -361,6 +349,7 @@ public class Auton extends LinearOpMode {
                                 elbow.elbowStraight(),// go to bucket and lift slides up
                                 Sample_1,
                                 Slides.slidesUp()
+
                         ),
                         // put the elbow up
                         new ParallelAction(
@@ -370,34 +359,26 @@ public class Auton extends LinearOpMode {
                         ),
                         // open claw and go back
                         claw.openClaw(),
-                        goBack,
+                        elbow.elbowStraight(),
                         new ParallelAction(
                                 // move the slides down and go to pick up sample 2
                                 Slides.slidesDown(),
                                 Sample_2
                         ),
-                        new ParallelAction(
-                                elbow.lowerElbow()
-                        ),
-
+                        elbow.lowerElbow(),
                         claw.closeClaw(),
                         elbow.elbowStraight(),
                         new ParallelAction(
                                 Drop2,
                                 Slides.slidesUp()
                         ),
-                        new SequentialAction(
-                                claw.closeClaw()
-                        ),
-                        moveForward,
-                        //elbow.elbowUp(),
                         new ParallelAction(
                                 Slides.holdSlides(),
                                 elbow.elbowUp(),
                                 claw.openClaw()
                         ),
                         claw.openClaw(),
-                        goBack2,
+                        elbow.elbowStraight(),
                         new ParallelAction(
                                 Slides.slidesDown(),
                                 Sample_3
@@ -411,7 +392,7 @@ public class Auton extends LinearOpMode {
                                 Drop3,
                                 Slides.slidesUp()
                         ),
-                        moveForward3,
+
                         claw.closeClaw(),
                         new ParallelAction(
                                 Slides.holdSlides(),
@@ -419,7 +400,7 @@ public class Auton extends LinearOpMode {
                                 claw.openClaw()
                         ),
                         claw.openClaw(),
-                        goBack3,
+                        elbow.elbowStraight(),
                         new ParallelAction(
                                 Slides.slidesDown(),
                                 Sample_4
@@ -433,7 +414,6 @@ public class Auton extends LinearOpMode {
                                 Drop4,
                                 Slides.slidesUp()
                         ),
-                        moveForward4,
                         claw.closeClaw(),
                         new ParallelAction(
                                 Slides.holdSlides(),
@@ -441,10 +421,8 @@ public class Auton extends LinearOpMode {
                                 claw.openClaw()
                         ),
                         claw.openClaw(),
+                        elbow.elbowStraight(),
                         goBack4
-
-
-
 
                 ));
 
