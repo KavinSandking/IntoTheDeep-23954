cb508898defe71d6acdf028d9f585f201f4bcaa2
KavinSandking <150557351+KavinSandking@users.noreply.github.com>
2025-04-21 11:06:31 -0500

    1. Auton - updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/teleop/BasicRobot2526.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/teleop/BasicRobot2526.java
@@ -7,16 +7,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 
-import org.firstinspires.ftc.teamcode.roadrunner.subsystems.slides;
+
 
 @Config
-@slides.Attach
-@Disabled
 @TeleOp(name="BasicRobot2526")
 public class BasicRobot2526 extends OpMode {
     private DcMotorEx Intake2= null;
@@ -28,18 +27,22 @@ public class BasicRobot2526 extends OpMode {
     private DcMotorEx Intake = null;
     DcMotor Act1 = null;
     DcMotor Act2= null;
-    Servo elbow = null;
+    //Servo elbow = null;
     Servo elbow_2 = null;
     boolean isClawOpen = false;
     boolean wasXPressed = false; // Track if X was previously pressed
     boolean iselbowOpen = false;
     boolean wasBPressed = false; //toggle elbow when B pressed
     private PIDController controller;
-    public static double kp = 0.0007, ki = 0, kd = 0.0001;
-    public static double kf = 0.001;
-    public static double target = -4000;
+    public static double kp = 0.01, ki = 0, kd = 0.01;
+    public static double kf = 0.000;
+    public static double target = 0;
     private final double encoderTicksInDegrees = 537.7;
     private boolean isPidActive = false;
+
+    public double elbow2UpPos = 1;
+
+    public double elbow2downPos =0.675;
 
 
 
@@ -55,10 +58,16 @@ public class BasicRobot2526 extends OpMode {
         Intake = hardwareMap.get(DcMotorEx.class, "Slides");
         Intake2 = hardwareMap.get(DcMotorEx.class, "Slides2");
         claw = hardwareMap.servo.get("claw");
-        elbow = hardwareMap.servo.get("elbow");
+        //elbow = hardwareMap.servo.get("elbow");
         elbow_2 = hardwareMap.servo.get("elbow_2");
         Act1 = hardwareMap.dcMotor.get("Act1");
         Act2 = hardwareMap.dcMotor.get("Act2");
+
+
+        Intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        Intake2.setDirection(DcMotorSimple.Direction.REVERSE);
+        //elbow.setDirection(Servo.Direction.REVERSE);
+
 
         controller = new PIDController(kp, ki, kd);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
@@ -71,44 +80,28 @@ public class BasicRobot2526 extends OpMode {
 
     @Override
     public void loop() {
+
         Intake.setPower(0);
         FrontRight.setPower(0);
         BackLeft.setPower(0);
         FrontLeft.setPower(0);
         BackRight.setPower(0);
-        Intake2.setPower(0);
 
-
-        if (gamepad1.a) {
-            FrontLeft.setPower(-0.3);
-            BackLeft.setPower(-0.3);
-            FrontRight.setPower(-0.3);
-            BackRight.setPower(-0.3);
-        }
-        if (gamepad1.y) {
-            FrontLeft.setPower(0.3);
-            BackLeft.setPower(0.3);
-            FrontRight.setPower(0.3);
-            BackRight.setPower(0.3);
+        if (gamepad1.left_stick_y!=0.0){
+            FrontLeft.setPower(-gamepad1.left_stick_y);
+            BackLeft.setPower(-gamepad1.left_stick_y);
+        }else{
+            FrontLeft.setPower(0);
+            BackLeft.setPower(0);
         }
 
-
-        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
-        double x = gamepad1.left_stick_x * 1.1;
-        double rx = gamepad1.right_stick_x;
-
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double FrontLeftPower = (y + x + rx) / denominator;
-        double BackLeftPower = (y - x + rx) / denominator;
-        double FrontRightPower = (y - x - rx) / denominator;
-        double BackRightPower = (y + x - rx) / denominator;
-
-        FrontLeft.setPower(FrontLeftPower);
-        FrontRight.setPower(FrontRightPower);
-        BackRight.setPower(BackRightPower);
-        BackLeft.setPower(BackLeftPower);
-
-
+        if (gamepad1.right_stick_y!=0.0){
+            FrontRight.setPower(-gamepad1.right_stick_y);
+            BackRight.setPower(-gamepad1.right_stick_y);
+        }else{
+            FrontRight.setPower(0);
+            FrontLeft.setPower(0);
+        }
         if (gamepad1.right_bumper) {
             FrontLeft.setPower(0.75);
             BackLeft.setPower(-0.75);
@@ -121,6 +114,7 @@ public class BasicRobot2526 extends OpMode {
             BackRight.setPower(-0.75);
 
         }
+
         if (gamepad2.right_stick_y == 1.0) {
             Intake.setPower(1);
             Intake2.setPower(0.82);
@@ -157,20 +151,24 @@ public class BasicRobot2526 extends OpMode {
             }
         }
 
-        if (gamepad2.b && !wasBPressed){
+        /*if (gamepad2.b && !wasBPressed){
             iselbowOpen = !iselbowOpen;
             if (iselbowOpen){
-                elbow.setPosition(0.45);
-                elbow_2.setPosition(0.645);
+                elbow.setPosition(elbow1UpPos);
+                elbow_2.setPosition(elbow2UpPos);
             } else {
                 elbow.setPosition(0.188);
                 elbow_2.setPosition(0.878);
             }
+        }*/
+        if (gamepad2.b){
+            //elbow.setPosition(elbow1UpPos);
+            elbow_2.setPosition(elbow2UpPos);
         }
         // 0.45, 0.645
         if (gamepad2.y) {
-            elbow.setPosition(0.5);
-            elbow_2.setPosition(0.45);
+            //elbow.setPosition(elbow1downPos);
+            elbow_2.setPosition(elbow2downPos);
 
             // Add telemetry for debugging
         }
